29203e7e07ec3a40bbc0d57fe80735a4132a67ee
erffy <ceyhunerenerguven.07@proton.me>
2024-12-04 23:35:49 +0300

    chore: use '??' operator instead of '||'

--- firefox/src/lib/init_default_settings.ts
+++ firefox/src/lib/init_default_settings.ts
@@ -4,10 +4,10 @@ export const InitDefaultSettings = async (): Promise<Settings> => {
     const settings_in_storage = (await browser.storage.local.get('firerss_settings')).firerss_settings;
 
     const settings: Settings = {
-        color_scheme: settings_in_storage?.theme || 'light',
-        theme: settings_in_storage?.theme || 'default',
-        ignored_sites: settings_in_storage?.ignored_sites || ['(.*).hetzner.(.*)'],
-        extended_feed_scan: settings_in_storage?.extended_feed_scan || 0,
+        color_scheme: settings_in_storage?.theme ?? 'light',
+        theme: settings_in_storage?.theme ?? 'default',
+        ignored_sites: settings_in_storage?.ignored_sites ?? ['(.*).hetzner.(.*)'],
+        extended_feed_scan: settings_in_storage?.extended_feed_scan ?? 0,
     };
 
     browser.storage.local.set({ firerss_settings: settings });
