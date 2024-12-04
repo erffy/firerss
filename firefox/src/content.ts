631f278825dca6ff30a57db9f6aba17dbbf0b2b9
erffy <ceyhunerenerguven.07@proton.me>
2024-12-04 23:13:05 +0300

    chore: some optimizations

--- firefox/src/content.ts
+++ firefox/src/content.ts
@@ -1,69 +1,56 @@
 import { InitDefaultSettings } from './lib/init_default_settings';
 
 export const findAllFeeds = async (): Promise<string[]> => {
-    const settings =
-        (await browser.storage.local.get('firerss_settings')).firerss_settings ?? (await InitDefaultSettings());
-    const feed_urls: string[] = [];
-    const youtube_user_pattern = /(?<=(https:\/\/(www\.)?youtube.com\/))@\w+/gi;
-    let doc: Document;
+  const settings = (await browser.storage.local.get('firerss_settings')).firerss_settings ?? await InitDefaultSettings();
+  const feed_urls = new Set<string>();
+  const youtube_user_pattern = /(?<=(https:\/\/(www\.)?youtube.com\/))@\w+/gi;
 
-    if (window.location.origin.includes('youtube.com') && window.location.href.match(youtube_user_pattern)) {
-        doc = new DOMParser().parseFromString(
-            await (await fetch(window.location.href + '/about', { mode: 'cors' })).text(),
-            'text/html'
-        );
+  let doc: Document;
+  if (window.location.origin.includes('youtube.com') && window.location.href.match(youtube_user_pattern)) doc = new DOMParser().parseFromString(document.documentElement.outerHTML, 'text/html');
+  else doc = document;
+
+  const collected_feeds = doc.querySelectorAll('link[type="application/rss+xml"], link[type="application/atom+xml"], a[href*="rss"], a[href*="atom"], a[href*="feed"]');
+  for (let index = 0, feed = collected_feeds[index]; index < collected_feeds.length; index++) {
+    if (feed instanceof HTMLLinkElement) {
+      if (feed.href.includes('youtube.com') && !feed.baseURI.match(youtube_user_pattern)) return [];
+      feed_urls.add(feed.href);
+    }
+  }
+
+  if (settings.extended_feed_scan === 0) return Array.from(feed_urls);
+  else if (settings.extended_feed_scan === 1) {
+    if (feed_urls.size > 0) return Array.from(feed_urls);
+  }
+
+  const possible_feed_files = [
+    '.atom',
+    '.feed',
+    '.rss',
+    '.xml',
+    '/atom.xml',
+    '/atom',
+    '/feed.xml',
+    '/feed',
+    '/index.xml',
+    '/rss.xml',
+    '/rss',
+  ];
+
+  for (const file of possible_feed_files) {
+    let feed;
+    if (window.location.pathname === '/') {
+      if (file.startsWith('.')) continue;
+      feed = window.location.href + file.slice(1);
     } else {
-        doc = document;
+      if (file.startsWith('.')) feed = window.location.href + file;
+      else feed = window.location.origin + file;
     }
 
-    const collected_feeds = doc.querySelectorAll(
-        'link[type="application/rss+xml"], link[type="application/atom+xml"], a[href*="rss"], a[href*="atom"], a[href*="feed"]'
-    );
-
-    for (let i = 0; i < collected_feeds.length; i++) {
-        const feed = collected_feeds[i];
-        if (feed instanceof HTMLLinkElement) {
-            if (feed.href.includes('youtube.com') && !feed.baseURI.match(youtube_user_pattern)) continue;
-            feed_urls.push(feed.href);
-        }
+    const response = await fetch(feed);
+    if (response.ok && response.headers.get('Content-Type')?.includes('xml') && !feed_urls.has(feed)) {
+      feed_urls.add('_' + feed);
     }
+  }
 
-    if (settings.extended_feed_scan === 0) {
-        return feed_urls;
-    } else if (settings.extended_feed_scan === 1) {
-        if (feed_urls.length > 0) return feed_urls;
-    }
-    const possible_feed_files = [
-        '.atom',
-        '.feed',
-        '.rss',
-        '.xml',
-        '/atom.xml',
-        '/atom',
-        '/feed.xml',
-        '/feed',
-        '/index.xml',
-        '/rss.xml',
-        '/rss',
-    ];
-    for (const file of possible_feed_files) {
-        let feed;
-        if (window.location.pathname === '/') {
-            if (file.startsWith('.')) continue;
-            feed = window.location.href + file.slice(1);
-        } else {
-            if (file.startsWith('.')) {
-                feed = window.location.href + file;
-            } else {
-                feed = window.location.origin + file;
-            }
-        }
-        const response = await fetch(feed);
-        if (response.ok && response.headers.get('Content-Type')?.includes('xml') && !feed_urls.includes(feed)) {
-            feed_urls.push('_' + feed);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 700));
-    }
-
-    return feed_urls;
+  return Array.from(feed_urls);
 };
--- firefox/src/popup.ts
+++ firefox/src/popup.ts
@@ -5,76 +5,70 @@ import { Settings } from './types/settings_interface';
 let settings: Settings;
 
 const initPopup = async () => {
-    ApplyColorScheme(settings.color_scheme);
-    ApplyTheme(settings.theme);
+	if (!settings) settings = (await browser.storage.local.get('firerss_settings')).firerss_settings;
 
-    const url = new URL(window.location.href);
-    const feed_urls: string[] = JSON.parse(url.searchParams.get('feedlinks'));
-    const feed_list = document.getElementById('feed_url_list');
+	ApplyColorScheme(settings.color_scheme);
+	ApplyTheme(settings.theme);
 
-    document.getElementById('settings_button').addEventListener('click', () => {
-        browser.runtime.openOptionsPage();
-    });
+	const url = new URL(window.location.href);
+	const feedUrls: string[] = JSON.parse(url.searchParams.get('feedlinks'));
 
-    for (let feed_url of feed_urls) {
-        const tr = document.createElement('tr');
-        const td1 = document.createElement('td');
-        const td2 = document.createElement('td');
-        const td3 = document.createElement('td');
-        const a = document.createElement('a');
-        const info_div = document.createElement('div');
-        const copy_btn = document.createElement('button');
+	const feedList = document.getElementById('feed_url_list');
+	const settingsButton = document.getElementById('settings_button');
 
-        if (feed_url.startsWith('_')) {
-            info_div.setAttribute('class', 'help-icon');
-            info_div.setAttribute('aria-label', 'This feed was found by Extended Feed Scan');
-            info_div.innerText = '!';
-            feed_url = feed_url.slice(1);
-        }
+	settingsButton?.addEventListener('click', () => browser.runtime.openOptionsPage());
 
-        td1.setAttribute('class', 'col1 feed_url');
-        td2.setAttribute('class', 'col2 info_button_area');
-        td3.setAttribute('class', 'col3 copy_button_area');
-        a.setAttribute('class', 'feed_url_link');
-        a.setAttribute('target', '_blank');
-        a.setAttribute('href', feed_url);
-        a.setAttribute('title', feed_url);
-        a.innerText = feed_url;
-        copy_btn.setAttribute('class', 'copy_button');
-        copy_btn.setAttribute('title', 'Copy to clipboard');
-        copy_btn.setAttribute('value', feed_url);
-        copy_btn.innerText = 'Copy';
+	for (let feedUrl of feedUrls) {
+		const tr = document.createElement('tr');
+		const td1 = document.createElement('td');
+		const td2 = document.createElement('td');
+		const td3 = document.createElement('td');
+		const a = document.createElement('a');
+		const infoDiv = document.createElement('div');
+		const copyBtn = document.createElement('button');
 
-        td1.appendChild(a);
-        if (info_div.innerText) {
-            td2.appendChild(info_div);
-            td2.appendChild(document.createTextNode('\u00A0'));
-        }
-        td3.appendChild(copy_btn);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        feed_list.appendChild(tr);
-    }
+		if (feedUrl.startsWith('_')) {
+			infoDiv.setAttribute('class', 'help-icon');
+			infoDiv.setAttribute('aria-label', 'This feed was found by Extended Feed Scan');
+			infoDiv.innerText = '!';
+			feedUrl = feedUrl.slice(1);
+		}
 
-    const copy_buttons = document.querySelectorAll('.copy_button');
-    for (const button of Array.from(copy_buttons)) {
-        button.addEventListener('click', async (event) => {
-            const feed_url = (event.target as HTMLButtonElement).value;
-            await navigator.clipboard.writeText(feed_url);
-            (button as HTMLButtonElement).innerText = 'Copied!';
-            setTimeout(() => {
-                (button as HTMLButtonElement).innerText = 'Copy';
-            }, 2000);
-        });
-    }
+		td1.setAttribute('class', 'col1 feed_url');
+		td2.setAttribute('class', 'col2 info_button_area');
+		td3.setAttribute('class', 'col3 copy_button_area');
+		a.setAttribute('class', 'feed_url_link');
+		a.setAttribute('target', '_blank');
+		a.setAttribute('href', feedUrl);
+		a.setAttribute('title', feedUrl);
+		a.innerText = feedUrl;
+		copyBtn.setAttribute('class', 'copy_button');
+		copyBtn.setAttribute('title', 'Copy to clipboard');
+		copyBtn.setAttribute('value', feedUrl);
+		copyBtn.innerText = 'Copy';
+
+		td1.appendChild(a);
+		if (infoDiv.innerText) {
+			td2.appendChild(infoDiv);
+			td2.appendChild(document.createTextNode('\u00A0'));
+		}
+		td3.appendChild(copyBtn);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		feedList.appendChild(tr);
+	}
+
+	const copyButtons = document.querySelectorAll('.copy_button');
+	for (let index = 0, button = copyButtons[index] as HTMLButtonElement; index < copyButtons.length; index++) button.addEventListener('click', async (event) => {
+		await navigator.clipboard.writeText((event.target as HTMLButtonElement).value);
+		button.innerText = 'Copied!';
+		setTimeout(() => button.innerText = 'Copy', 2000);
+	});
 };
 
 (async () => {
-    settings = (await browser.storage.local.get('firerss_settings')).firerss_settings;
-    if (document.readyState === 'interactive' || document.readyState === 'complete') {
-        initPopup();
-    } else {
-        document.addEventListener('DOMContentLoaded', initPopup, { once: true });
-    }
+	settings = (await browser.storage.local.get('firerss_settings')).firerss_settings;
+	if (document.readyState === 'interactive' || document.readyState === 'complete') await initPopup();
+	else document.addEventListener('DOMContentLoaded', initPopup, { once: true });
 })();
